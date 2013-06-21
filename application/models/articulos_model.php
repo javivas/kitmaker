b36ec422186c12e6dd149a908261562e7e742392
javivas <javivas@192.168.1.17>
2013-06-21 11:13:40 +0200

    Añadido comentarios al model

--- application/models/articulos_model.php
+++ application/models/articulos_model.php
@@ -1,27 +1,37 @@
 <?php
-
+/**
+* Articulos_Model
+*
+* @uses     CI_Model
+* @package  Kitmaker
+* @version  1.0
+* @author   javivas
+*
+* Description: Gestión de Articulos/posts
+*
+*/
 class Articulos_model extends CI_Model {
-
+    //Todos los artículos en la BBDD
     function get_all_articulos() {
         $query = $this->db->query("SELECT * FROM articulos ORDER BY id DESC");
         return $query->result();
     }
-
+    //Todos los artículos en la BBDD que con pública a true
     function get_public_articulos() {
         $query = $this->db->query("SELECT * FROM articulos WHERE publica=true ORDER BY id DESC");
         return $query->result();
     }
-
+    //Los últimos 5 artículos en la BBDD que con pública a true
     function get_last_articulos() {
         $query = $this->db->query("SELECT * FROM articulos WHERE publica=true ORDER BY id DESC LIMIT 5");
         return $query->result();
     }
-
+    // El artículo con id = $id
     function get_articulo($id) {
         $query = $this->db->query("SELECT * FROM articulos WHERE id=" . $id);
         return $query->result();
     }
-
+    //insertar un artículo
     function insert_articulo($tit = null, $seccion = null, $cuerpo = null, $publica = null) {
         try {
             $data = array(
@@ -37,7 +47,7 @@ class Articulos_model extends CI_Model {
             return false;
         }
     }
-
+    //Obtener el valor del atributo publica de un artículo con id = $id
     function get_visibilidad_articulo($id) {
         $query = $this->db->query("SELECT publica FROM articulos WHERE id=" . $id);
         if ($query->num_rows() > 0) {
@@ -48,7 +58,7 @@ class Articulos_model extends CI_Model {
             return null;
         }
     }
-
+    //Cambiar el valor del atributo publica de un artículo con id= $id al valor $pub
     function visibilidad($id = null, $pub = null) {
 
         try {
